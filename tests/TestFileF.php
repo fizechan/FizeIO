7d2b40dc7f64eb74582ee837463d5e2b277466e5
FizeChan <chenfengzhan@qq.com>
2021-08-05 23:04:14 +0800

    FileF完成单元测试。

--- tests/TestFileF.php
+++ tests/TestFileF.php
@@ -6,33 +6,49 @@ use PHPUnit\Framework\TestCase;
 class TestFileF extends TestCase
 {
 
-    public function testOpen()
-    {
-
-    }
-
     public function test__destruct()
     {
+        $file1 = new FileF();
+        $file1->open(__DIR__ . '/../temp/test.txt', 'w+');
+        unset($file1);
 
-    }
-
-    public function testPutcsv()
-    {
-
+        $file = new FileF();
+        $file->open(__DIR__ . '/../temp/test.txt', 'w+');
+        self::assertTrue(true);
     }
 
     public function testClose()
     {
-
-    }
-
-    public function test__construct()
-    {
-
+        $file = new FileF();
+        $file->open(__DIR__ . '/../temp/test.txt', 'w+');
+        $rst = $file->close();
+        self::assertTrue($rst);
     }
 
     public function testGetcsv()
     {
+        $file = new FileF();
+        $file->open(__DIR__ . '/../temp/data/test.csv', 'r');
+        $csv = $file->getcsv();
+        var_dump($csv);
+        self::assertIsArray($csv);
+    }
 
+    public function testPutcsv()
+    {
+        $file = new FileF();
+        $file->open(__DIR__ . '/../temp/csv.csv', 'w+');
+        $fields = ['汽车', 'VIN码查询', 'Vin/query', '根据VIN码查询车辆相关信息', '聚合', '通过'];
+        $len = $file->putcsv($fields);
+        var_dump($len);
+        self::assertIsInt($len);
+        $file->close();
+    }
+
+    public function testOpen()
+    {
+        $file = new FileF();
+        $file->open(__DIR__ . '/../temp/test.txt', 'w+');
+        self::assertTrue(true);
     }
 }
